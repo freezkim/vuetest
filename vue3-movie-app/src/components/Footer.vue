f15cd34b652662bed74fb7264f6caadea90eb004
freezkim <freezkim@gmail.com>
2021-05-24 19:29:06 +0900

    movie detail layout 추가, loading animation 추가

---
+++ vue3-movie-app/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <Logo />
+        <a
+            href="https://freezkim.tistory.com"
+            target_= "_blank">
+            (c){{ new Date().getFullYear() }} freezkim
+        </a>
+    </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+    components:    {
+        Logo
+    }
+}
+</script>
+
+<style lang = "scss" scoped>
+footer{
+    padding: 70px 0;
+    text-align: center;
+    opacity: .3;
+    .logo {
+        display: block;
+        margin-bottom: 4px;
+    }
+}
+</style>
