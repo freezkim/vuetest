0e6865c07d37335a3fc89c295327463c0b729bdf
freezkim <freezkim@gmail.com>
2021-05-25 19:42:24 +0900

    add test add opengraph

---
+++ vue3-movie-app/tests/example.test.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+// import Example from './Parent.vue'
+// TypeError: Cannot destructure property 'config' of 'undefined' as it is undefined.
+
+test('메세지를 변경합니다.', () => {
+    // expect('1').toBe('1')
+    // const wrapper = mount(Example)
+    // wrapper.vm === this
+    // expect(wrapper.vm.msg).toBe('Hello Vue test utils!')
+})
